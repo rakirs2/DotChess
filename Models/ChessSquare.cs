62bbc8dd9e86c406a8cccbbd5c5b98b042ab1ece
Srikar Mylavarapu <Srikar.mylavarapu@gmail.com>
2022-06-30 15:30:51 -0500

    removing currently unnecessary fields in ChessSquare

--- Models/ChessSquare.cs
+++ Models/ChessSquare.cs
@@ -2,11 +2,9 @@
 {
     public class ChessSquare
     {
-        public char rank { get; set; }
-        public int file { get; set; }
+
         public Boolean occupied { get; set; }
-        public Boolean isWhiteOccupied { get; set; }
-        public Boolean isBlackOccupied { get; set; }
+
 
         public ChessSquare()
         {
