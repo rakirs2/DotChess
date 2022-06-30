a10aaf1973a8cfa6e0e3eab862c6adcc164e4037
Srikar Mylavarapu <Srikar.mylavarapu@gmail.com>
2022-06-30 11:23:47 -0500

    populate files --> add not append

--- ChessBoard.cs
+++ ChessBoard.cs
@@ -4,14 +4,17 @@ namespace CSharpChess
 {
     internal class ChessBoard
     {
-        public List<ChessFile> files { get;}
+        public List<ChessFile> files { get; set; }
         public ChessBoard()
         {
-            //TODO populate files
-            //this.files = new List<ChessFile>(8);
+            //TODO override to strings to print out a chess board
+
+            this.files = new List<ChessFile>(8);
+
             for (int i = 0; i < 8; i++)
             {
-                Console.WriteLine(i);
+                this.files.Add(new ChessFile());
+                Console.WriteLine(this.files.Count);
             }
         } 
 
