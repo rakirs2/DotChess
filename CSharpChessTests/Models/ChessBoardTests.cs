d34f07529ffd9ca738148d4d087c0deac80f940e
Srikar Mylavarapu <Srikar.mylavarapu@gmail.com>
2022-07-06 09:56:31 -0500

    fixing repo to include tests

---
+++ CSharpChessTests/Models/ChessBoardTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpChess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpChess.Models;
+
+namespace CSharpChess.Tests
+{
+    [TestClass()]
+    public class ChessBoardTests
+    {
+        [TestMethod]
+        public void ChessBoardDimensions()
+        {
+            ChessBoard board = new ChessBoard();
+            Assert.AreEqual(board.Files.Count, 8);
+            Assert.AreEqual(board.Files[0].ranks.Count, 8);
+        }
+    }
+}
