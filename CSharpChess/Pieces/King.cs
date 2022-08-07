c510578560d9438d0d8f9a7a40bffb6b8babefa7
rakirs <srikar.mylavarapu@gmail.com>
2022-08-07 08:48:29 -0500

    updating

--- CSharpChess/Pieces/King.cs
+++ CSharpChess/Pieces/King.cs
@@ -3,15 +3,22 @@ namespace CSharpChess.Pieces
 {
     public class King : Piece
     {
-        Boolean m_color;
+        bool Piece.m_color { get; set; }
         private Position m_currentPosition;
 
         public King(int rank, int file, Boolean color) 
         {
-            m_color = color;
+            Piece.m_color = color;
             m_currentPosition = new Position(rank, file);
         }
+
         
+        Position Piece.m_currentPosition { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        bool Piece.move(int newRank, int newFile)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
 
