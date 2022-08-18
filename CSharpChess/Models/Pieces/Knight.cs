be59f71f5a783267e9a8a8e15267801520a0ece5
rakirs2 <srmylavarapu@microsoft.com>
2022-08-17 20:59:22 -0500

    adding knight moves

---
+++ CSharpChess/Models/Pieces/Knight.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpChess.Models.Pieces
+{
+    public class Knight : IPiece
+    {
+        private readonly Color _color;
+        private readonly PieceType _pieceType;
+        private Position _position;
+        public Knight(Color color, Position position)
+        {
+            _color = color;
+            _pieceType = PieceType.Knight;
+            _position = position;
+        }
+        public Color GetColor()
+        {
+            return _color;
+        }
+
+        public Position GetCurrentPosition()
+        {
+            return _position;
+        }
+
+        public PieceType GetPieceType()
+        {
+            return _pieceType;
+        }
+
+        public HashSet<Position> GetPossibleMoves()
+        {
+            //assuming 3,3
+            HashSet<Position> possibleMoves = new HashSet<Position>();
+            //5,4
+            if (BoardUtils.IsOnBoard(_position.Rank + 2, _position.File +1))
+            {
+                possibleMoves.Add(new Position(_position.Rank + 2, _position.File +1));
+            }
+            //4,5
+            if (BoardUtils.IsOnBoard(_position.Rank + 1, _position.File + 2))
+            {
+                possibleMoves.Add(new Position(_position.Rank +1, _position.File + 2));
+            }
+            //2,5
+            if (BoardUtils.IsOnBoard(_position.Rank-1, _position.File + 2))
+            {
+                possibleMoves.Add(new Position(_position.Rank-1, _position.File + 2));
+            }
+            //1,4
+            if (BoardUtils.IsOnBoard(_position.Rank - 2, _position.File + 1))
+            {
+                possibleMoves.Add(new Position(_position.Rank - 2, _position.File + 1));
+            }
+            //1,2
+            if (BoardUtils.IsOnBoard(_position.Rank - 2, _position.File -1))
+            {
+                possibleMoves.Add(new Position(_position.Rank - 2, _position.File-1));
+            }
+            //2,1
+            if (BoardUtils.IsOnBoard(_position.Rank - 1, _position.File - 2))
+            {
+                possibleMoves.Add(new Position(_position.Rank - 1, _position.File - 2));
+            }
+            //4,1
+            if (BoardUtils.IsOnBoard(_position.Rank+1, _position.File - 2))
+            {
+                possibleMoves.Add(new Position(_position.Rank+1, _position.File - 2));
+            }
+            //5,2
+            if (BoardUtils.IsOnBoard(_position.Rank + 2, _position.File - 1))
+            {
+                possibleMoves.Add(new Position(_position.Rank + 2, _position.File - 1));
+            }
+
+            return possibleMoves;
+
+        }
+    }
+}
---
+++ CSharpChessTests/Models/Pieces/KnightTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpChess.Models.Pieces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpChess.Pieces;
+
+namespace CSharpChess.Models.Pieces.Tests
+{
+    [TestClass()]
+    public class KnightTests
+    {
+        [TestMethod()]
+        public void KnightIdentityTest()
+        {
+            Knight black = new(Color.Black, new Position(0, 0));
+            Knight white = new(Color.White, new Position(0, 0));
+            Assert.AreEqual(black.GetPieceType(), PieceType.Knight);
+            Assert.AreEqual(black.GetPieceType(), white.GetPieceType());
+            Assert.AreEqual(black.GetColor(), Color.Black);
+            Position position = new(0, 0);
+            Assert.AreEqual(position, black.GetCurrentPosition());
+        }
+
+        [TestMethod()]
+        public void GetPossibleMovesTestCenter()
+        {
+            Knight black = new(Color.Black, new Position(3, 3));
+            HashSet<Position> possibleMoves = black.GetPossibleMoves();
+            HashSet<Position> expectedMoves = new()
+            {
+                new Position(5, 4),
+                new Position(4, 5),
+                new Position(2, 5),
+                new Position(1, 4),
+                new Position(1, 2),
+                new Position(4, 1),
+                new Position(5, 2),
+                new Position(4, 1)
+            };
+            Assert.IsTrue(possibleMoves.SetEquals(expectedMoves));
+        }
+
+        [TestMethod()]
+        public void GetPossibleMovesTest3Options()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void GetPossibleMovesTestCorner()
+        {
+            Assert.Fail();
+        }
+
+    }
+}
