ace5f71168b79ce924e7782d57d49f51e85a7a1d
rakirs2 <srmylavarapu@microsoft.com>
2023-04-22 07:39:52 -0400

    fixing

--- DotChess/Console.cs
+++ DotChess/Console.cs
@@ -2,5 +2,30 @@
 {
     internal class Console
     {
+        public Console()
+        {
+            System.Console.WriteLine("Welcome To DotChess");
+            bool continueProgram = true;
+            while (continueProgram)
+            {
+                string input = System.Console.ReadLine().ToLower();
+                if (input == null || input == EndGame)
+                {
+                    continueProgram = false;
+                }
+
+                if (InterfacesSupported.Contains(input))
+                {
+
+                }
+            } 
+
+        }
+
+
+        private HashSet<String> InterfacesSupported = new HashSet<String>() { "uci" };
+        private HashSet<String> Modes = new HashSet<String>() { "debug" };
+        private HashSet<String> state = new HashSet<String>() { "isready" };
+        private String EndGame = "quit";
     }
 }
